b071b8f0aa4b696cca64d11eaaca7121570e9c9d
neos55555 <778007921@qq.com>
2020-07-29 09:50:28 +0800

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'antd/dist/antd.css';
+import 'medium-editor/dist/css/medium-editor.css'
+
+import 'braft-editor/dist/index.css'
+import 'braft-extensions/dist/code-highlighter.css'
+import 'braft-extensions/dist/color-picker.css'
+import 'braft-extensions/dist/table.css'
+
+import './App.scss';
+import Routers from '@/common/Routers'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routers />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,13 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { ConfigProvider } from 'antd';
+import zhCN from 'antd/es/locale/zh_CN';
+import { Provider } from 'react-redux';
+import store from '@/store/reducer'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+	<ConfigProvider locale={zhCN}>
+	  <Provider store={store}>
+    	<App />
+	  </Provider>
+
+  </ConfigProvider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
